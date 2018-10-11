28847f15a06062b828b0fc6b6145050ce8329dcc
Sam Ticknor <ticknors@carnegiemuseums.org>
2018-10-11 12:16:44 -0400

    Added 2015 drawings

--- grid.js
+++ grid.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  var viewer = ImageViewer();
+  
   $(".grid").each(function(index) {
     grid = $($(".grid")[index]);
     var $grid = grid.masonry({
@@ -14,9 +16,10 @@ $(document).ready(function() {
   });
 
    $(".grid-item").click(function() {
-    var viewer = ImageViewer();
-    var lowResolutionImage = $(this).data("low-res");
-    var highResolutionImage = $(this).data("high-res");
-    viewer.show(lowResolutionImage, highResolutionImage);
+    if ($(this).hasClass("clickable")) {
+      var lowResolutionImage = $(this).data("low-res");
+      var highResolutionImage = $(this).data("high-res");
+      viewer.show(lowResolutionImage, highResolutionImage);
+    }
   });
 });
